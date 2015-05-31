f7e7bc106b5e4a316bd179e61107ba601eb683a4
CrowsT <pt.wenhan@gmail.com>
2015-05-31 19:17:49 +0800

    确立了基本的文件结构。简单得创建了主页

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('homepage.urls', namespace='home')),
     url(r'^admin/', include(admin.site.urls)),
 ]
