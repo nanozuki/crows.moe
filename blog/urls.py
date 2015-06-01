ec7a8a528837d027044d8865dc6fa4236b12afd9
Malusbilis <pt.wenhan@gmail.com>
2015-06-02 02:44:09 +0800

    创建了简单的blog页面，包括分区首页category.html和文章的article.html。加入了导航条。

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<category_id>\d+)/$', views.category_view, name='category'),
-    url(r'^/article/(?P<article_id>\d+)/$', views.article_view, name='article')
+    url(r'^article/(?P<article_id>\d+)/$', views.article_view, name='article')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,19 @@ from .models import Article, Category
 
 
 def category_view(request, category_id):
-    category = get_object_or_404(Category, pk=category_id)
-    return render(request, 'blog/category.html', {'category': category})
+    _category = get_object_or_404(Category, pk=category_id)
+    category_list = Category.objects.all()
+    recent_update = Article.objects.order_by('-publish_time').filter(category=_category)
+    if len(recent_update) > 5:
+        recent_update = recent_update[:5]
+    return render(request, 'blog/category.html', {'category': _category,
+                                                  'category_list':category_list,
+                                                  'recent_update': recent_update})
 
 
 def article_view(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
-    return render(request, 'blog/article.html', {'article': article})
+    category_list = Category.objects.all()
+    return render(request, 'blog/article.html',
+                  {'article': article,
+                   'category_list': category_list})
