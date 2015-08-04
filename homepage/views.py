5070dcf8cfc7e153e151a90bfabadac4d114b90a
CrowsT <pt.wenhan@gmail.com>
2015-08-05 07:28:13 +0800

    缓慢调整界面中...时间太长无奈CI个。。

--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from blog.models import Category
-
+from blog.models import Article, Category, Tag
 
 def index(request):
-    category_list = Category.objects.all()
-    site_title = "烏鴉的庭院"
-    return render(request, 'homepage/index.html',
-                  {'category_list': category_list,
-                   'site_title': site_title})
+    try:
+        tag_aboutme = Tag.objects.get(tag_name='关于')
+        article_list = tag_aboutme.article_set.order_by("-last_update_time")
+        if len(article_list) > 0:
+            article = article_list[0]
+            article.clicks += 1
+            article.category.clicks += 1
+            tag_aboutme.clicks += 1
+            article.save()
+            article.category.save()
+            tag_aboutme.save()
+            return render(request, 'homepage/index.html', {
+                'article': article_list[0]
+            })
+        else:
+            return render(request, 'homepage/index.html')
+    except Tag.DoesNotExist:
+        return render(request, 'homepage/index.html')
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -22,4 +24,4 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^aboutme/', include('aboutme.urls', namespace='aboutme')),
     url(r'^msgboard/', include('msgboard.urls', namespace='msgboard')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
