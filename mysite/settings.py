0c360e4409a6938aadb463e21abd0a338213a988
CrowsT <pt.wenhan@gmail.com>
2015-10-18 21:49:22 +0800

    收集了admin所需的css

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,7 +109,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 
