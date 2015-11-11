d76168e5e1d3fe84a59b48e37a2d8cbfc5353c7b
CrowsT <pt.wenhan@gmail.com>
2015-11-11 16:42:35 +0800

    修复bug：文章标题显示的时间和显示关于的排序改为了「publish_time」

--- homepage/views.py
+++ homepage/views.py
@@ -4,7 +4,7 @@ from blog.models import Article, Category, Tag
 def index(request):
     try:
         tag_aboutme = Tag.objects.get(tag_name='关于')
-        article_list = tag_aboutme.article_set.order_by("-last_update_time")
+        article_list = tag_aboutme.article_set.order_by("-publish_time")
         if len(article_list) > 0:
             return render(request, 'homepage/index.html', {
                 'site_title': "乌鸦的庭院",
