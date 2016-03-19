5132343b05fe098bfc0961750f86638eb136fcc0
CrowsT <pt.wenhan@gmail.com>
2016-03-19 09:41:53 -0400

    将media文件迁出工程

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,7 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = private_settings.MEDIA_ROOT
 
 # Auth Settings
 LOGIN_URL = '/crows/login/'
