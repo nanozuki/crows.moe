48748627a1f0f52fefd9edb8b16df2eeb74b4c0a
CrowsT <pt.wenhan@gmail.com>
2016-03-20 09:55:09 +0800

    修复了一些bug，留言界面也增加验证码验证

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PrivateMessage, PublicMessage
+
 
 # Register your models here.
+admin.site.register(PrivateMessage)
+admin.site.register(PublicMessage)
--- contact/views.py
+++ contact/views.py
@@ -3,37 +3,57 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from blog.models import Category
 from .models import PublicMessage, PrivateMessage
+from .form import PostMessageForm
 
 
 def index(request):
     category_list = Category.objects.all()
     pub_msg_list = PublicMessage.objects.order_by("-timestamp")
+
+    name_value = request.session.get('name', '')
+    email_value = request.session.get('name', '')
+
+    form = PostMessageForm(initial={
+        'name': name_value, 'email': email_value})
+
     return render(request, 'contact/index.html', {
         'category_list': category_list,
         'pub_msg_list': pub_msg_list,
-        'name': request.session.get('name', ""),
-        'email': request.session.get('email', "")
+        'post_msg_form': form,
     })
 
 
 def post_msg(request, reply_id=None):
-    name = request.POST['name']
-    email = request.POST['email']
-    content = request.POST['content']
-    target = -1
+    if request.method == 'POST':
+        form = PostMessageForm(request.POST)
+        if form.is_valid():
+            name = request.POST['name']
+            email = request.POST['email']
+            content = request.POST['content']
+            target = -1
 
-    if name == "":
-        name = "匿名访客"
+            if name == "":
+                name = "匿名访客"
 
-    if reply_id is not None:
-        target = reply_id
+            if reply_id is not None:
+                target = reply_id
 
-    request.session['name'] = name
-    request.session['email'] = email
+            request.session['name'] = name
+            request.session['email'] = email
 
-    msg = PublicMessage(name=name,
-                        email=email,
-                        content=content,
-                        reply_id=target)
-    msg.save()
-    return HttpResponseRedirect(reverse("contact:index"))
+            msg = PublicMessage(name=name,
+                                email=email,
+                                content=content,
+                                reply_id=target)
+            msg.save()
+            return HttpResponseRedirect(reverse("contact:index"))
+        else:
+            category_list = Category.objects.all()
+            pub_msg_list = PublicMessage.objects.order_by("-timestamp")
+            return render(request, 'contact/index.html', {
+                    'category_list': category_list,
+                    'pub_msg_list': pub_msg_list,
+                    'post_msg_form': form,
+            })
+    else:
+        return HttpResponseRedirect(reverse("contact:index"))
