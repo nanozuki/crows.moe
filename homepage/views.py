75981e84ca5dc3894ddfecb2815996e5917a9378
CrowsT <pt.wenhan@gmail.com>
2016-03-22 07:39:57 +0800

    开始将CSS框架改为materialize

--- homepage/views.py
+++ homepage/views.py
@@ -1,22 +1,17 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from blog.models import Tag
+from blog.models import Article, Category
 
 
 def index(request):
-    try:
-        tag_about = Tag.objects.get(tag_name='关于')
-        article_list = tag_about.article_set.order_by("-publish_time")
-        if len(article_list) > 0:
-            return render(request, 'homepage/index.html', {
-                'site_title': "乌鸦的庭院",
-                'article': article_list[0],
-            })
-        else:
-            return render(request, 'homepage/index.html')
-    except Tag.DoesNotExist:
-        return render(request, 'homepage/index.html')
+    article = Article.objects.get(pk=1)
+    category_list = Category.objects.all()
+    return render(request, 'homepage/index.html', {
+        'site_title': "乌鸦的庭院",
+        'article': article,
+        'category_list': category_list
+    })
 
 
 def about(request):
