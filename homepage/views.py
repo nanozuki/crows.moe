d7eca3e555d1111423127dd6a66eee44ee340876
CrowsT <pt.wenhan@gmail.com>
2016-03-27 20:12:39 +0800

    整合了css文件。调整了一些界面上的问题。手机页面的container调整为100%

--- homepage/views.py
+++ homepage/views.py
@@ -1,19 +1,16 @@
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from blog.models import Article, Category
-
-import random
 
 
 def index(request):
     article_list = Article.objects.order_by('-publish_time')[:10]
-    #random.shuffle(article_list)
+    # random.shuffle(article_list)
     category_list = Category.objects.all()
     return render(request, 'homepage/index.html', {
         'site_title': "乌鸦的庭院",
         'article_list': article_list,
         'category_list': category_list,
+        'nav_active': 'blog',
     })
 
 
