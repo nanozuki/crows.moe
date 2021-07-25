126a27e5052cce6ce1ea43ad43d764c219814a53
Nanozuki Crows <nanozuki.crows@gmail.com>
2021-07-25 14:57:08 +0800

    use gatsby to rewrite

--- razzle.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  modify: (config) => {
-    config.resolve.modules.unshift(path.resolve(__dirname, './src'));
-    config.node = { fs: 'empty' };
-    return config;
-  },
-};
--- src/log.js
+++
@@ -1,8 +0,0 @@
-const log = {
-  /* eslint-disable no-console */
-  info: console.log,
-  error: console.error,
-  /* eslint-enable no-console */
-};
-
-export { log };
