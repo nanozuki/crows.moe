2eaef7ee2a438a3a75ba8e28a58892c1ff4f4d0a
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-02-05 17:11:28 +0800

    start to dev frontend

---
+++ vote2021/pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <header>
+        <p>2021年</p>
+        <p>Programmers' Exodus</p>
+        <p>
+          媒体艺术祭 <small>投票环节</small>
+        </p>
+      </header>
+      <main>
+        <section>
+          <h1>新投票</h1>
+          <p>请输入你的telegram ID或者昵称</p>
+          <label htmlFor="user id">
+            <input type="text" />
+          </label>
+        </section>
+        <section>
+          <h1>查看/修改投票</h1>
+          <p>请输入投票ID</p>
+          <label htmlFor="vote id">
+            <input type="text" />
+          </label>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
