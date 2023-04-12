26efe6ed4d02b8e001682e15fc8b68c63cd728f4
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-04-12 20:50:48 +0800

    feat(mediavote): deploy

--- terraform/mediavote-api.tf
+++ terraform/mediavote-api.tf
@@ -15,7 +15,7 @@ resource "google_cloud_run_service" "mediavote-api" {
     }
     spec {
       containers {
-        image = "asia-east1-docker.pkg.dev/crows-moe/images/mediavote-api:1.0.7"
+        image = "asia-east1-docker.pkg.dev/crows-moe/images/mediavote-api:1.0.8"
         env {
           name  = "MEDIAVOTE_ENV"
           value = "production"
