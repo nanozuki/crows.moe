c582068a2261e9d3c8c7625427c6e6c6bce5c55b
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-04-17 18:30:32 +0800

    fix: image tag for blog

--- terraform/blog.tf
+++ terraform/blog.tf
@@ -15,7 +15,7 @@ resource "google_cloud_run_service" "blog" {
     }
     spec {
       containers {
-        image = "asia-east1-docker.pkg.dev/crows-moe/images/blog:${var.deploy_tag}"
+        image = "asia-east1-docker.pkg.dev/crows-moe/images/blog:1.0.0"
         ports {
           container_port = 80
         }
