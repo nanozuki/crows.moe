e621d82d6baf0643bd7c0081dda20434953f7663
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-04-19 14:00:32 +0800

    refact: reorganize to mono-repository

--- cms/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '10f30a3238180c7a9288811d47a5510c'),
-    },
-  },
-});
