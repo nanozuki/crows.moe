01881fcd770278652a243ae0e909ce30997cd89f
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-01-26 16:42:53 +0800

    fix(ema): nomination item

--- service/ema/src/lib/comp/Nomination.svelte
+++ service/ema/src/lib/comp/Nomination.svelte
@@ -3,13 +3,9 @@
   export let work: Work;
 </script>
 
-<div class="flex flex-col wide:flex-row items-start wide:items-center px-4 py-2 bg-overlay rounded">
+<div class="flex flex-col items-start px-4 py-2 bg-overlay rounded">
   <p class="flex-1 font-serif text-2xl">{work.name}</p>
-  <div class="flex-1">
-    {#each subNamesOfWork(work) as name (name)}
-      <p class="text-subtle">
-        {name}
-      </p>
-    {/each}
-  </div>
+  {#each subNamesOfWork(work) as name (name)}
+    <p class="text-subtle">{name}</p>
+  {/each}
 </div>
