66dd11598f362cca042c717e743faec49de188c0
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-08-09 11:02:46 +0800

    refact: clean and update flake

--- flake.nix
+++ flake.nix
@@ -10,15 +10,8 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          config = { allowUnfree = true; allowUnfreePredicate = (_: true); };
-        };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            # terraform
-            terraform
-            terraform-ls
             # nodejs and typescript
             nodePackages.nodejs
             nodePackages.pnpm
@@ -28,24 +21,7 @@
             # linter
             nodePackages.eslint
             nodePackages.eslint_d
-            # svelte
-            nodePackages.svelte-language-server
-            nodePackages.svelte-check
-            # go
-            go
-            gofumpt # formatter
-            golangci-lint # linter
-            gomodifytags
-            gopls # language server
-            gotests
-            gotestsum
-            gotools
-            iferr
-            impl
           ];
-          shellHook = ''
-            export GOROOT="${pkgs.go}/share/go"
-          '';
         };
       };
     };
